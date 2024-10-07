d563a4ee37e51f4977bf6a40cc2437d344b67006
dd-mahn <ducmanh0801@gmail.com>
2024-10-07 14:18:49 +0700

    Init Flask app for UI

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
 from flask import Flask, request, jsonify
-from model import load_model  # Assume a function to load your trained model
-
-app = Flask(__name__)
-model = load_model('path_to_model.h5')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    image = request.files['image']
-    prediction = model.predict(image)
-    return jsonify({'prediction': prediction})
-
-if __name__ == '__main__':
-    app.run(debug=True)
