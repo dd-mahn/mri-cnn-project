7c4dcbac11e74882e1f4e41c40c34aa08bf79404
dd-mahn <ducmanh0801@gmail.com>
2024-10-14 02:39:14 +0700

    Change activation mode to "softmax", remove unused function

--- src/model.py
+++ src/model.py
@@ -2,25 +2,6 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.applications import ResNet50
 
-def residual_block(x, filters, kernel_size=3, stride=1, conv_shortcut=False):
-    shortcut = x
-    
-    if conv_shortcut:
-        shortcut = layers.Conv2D(filters, 1, strides=stride)(shortcut)
-        shortcut = layers.BatchNormalization()(shortcut)
-    
-    x = layers.Conv2D(filters, kernel_size, strides=stride, padding='same')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation('relu')(x)
-    
-    x = layers.Conv2D(filters, kernel_size, padding='same')(x)
-    x = layers.BatchNormalization()(x)
-    
-    x = layers.Add()([shortcut, x])
-    x = layers.Activation('relu')(x)
-    
-    return x
-
 def build_resnet(input_shape, num_classes):
     base_model = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
     
@@ -32,7 +13,7 @@ def build_resnet(input_shape, num_classes):
     x = base_model.output
     x = layers.GlobalAveragePooling2D()(x)
     x = layers.Dense(1024, activation='relu')(x)
-    outputs = layers.Dense(num_classes, activation='sigmoid')(x)
+    outputs = layers.Dense(num_classes, activation='softmax')(x)
     
     model = models.Model(inputs=base_model.input, outputs=outputs)
     
